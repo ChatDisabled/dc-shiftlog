78e969986b2241dbb94aa6c760c9ed2a5a83139a
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2022-05-15 16:14:36 +0200

    for people spamming money events

--- client.lua
+++ client.lua
@@ -1,7 +1,13 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local PlayerData = QBCore.Functions.GetPlayerData()
+
 RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
     TriggerServerEvent('qb-shiftlog:server:OnPlayerUnload')
 end)
 
 RegisterNetEvent('QBCore:Player:SetPlayerData', function(Player)
-    TriggerServerEvent('qb-shiftlog:server:SetPlayerData', Player)
+    if PlayerData.job.name ~= Player.job.name or PlayerData.job.onduty ~= Player.job.onduty then
+        TriggerServerEvent('qb-shiftlog:server:SetPlayerData', Player)
+        PlayerData = Player
+    end
 end)
