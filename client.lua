e455c5f3d08ef0e38d932aa8388b240c6c4160ee
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2022-05-15 17:32:11 +0200

    patch playerdata wackyness

--- client.lua
+++ client.lua
@@ -1,11 +1,17 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local PlayerData = QBCore.Functions.GetPlayerData()
+local PlayerData = nil
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
+    PlayerData = QBCore.Functions.GetPlayerData()
+end)
 
 RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
     TriggerServerEvent('qb-shiftlog:server:OnPlayerUnload')
 end)
 
 RegisterNetEvent('QBCore:Player:SetPlayerData', function(Player)
+    if not Player then return end
+    if not PlayerData then return end
     if PlayerData.job.name ~= Player.job.name or PlayerData.job.onduty ~= Player.job.onduty then
         TriggerServerEvent('qb-shiftlog:server:SetPlayerData', Player)
         PlayerData = Player
